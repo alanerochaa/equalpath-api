366fde69d8f3d1cc6467906867abf52f3a6465df
alanerochaa <alanerochaa@gmail.com>
2025-11-13 16:58:19 -0300

    Initial commit

---
+++ src/main/java/com/equalpath/EqualpathApiApplication.java
@@ -0,0 +1,13 @@
+package com.equalpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqualpathApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqualpathApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equalpath/EqualpathApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equalpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqualpathApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
